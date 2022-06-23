e1e693fd6172f1d06eb75523e7638a77bf3694e6
truespring <truespring1@gmail.com>
2022-06-23 21:44:56 +0900

    second commit

---
+++ src/main/java/com/example/keycloakspringbootadapter/KeycloakSpringBootAdapterApplication.java
@@ -0,0 +1,13 @@
+package com.example.keycloakspringbootadapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeycloakSpringBootAdapterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeycloakSpringBootAdapterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/keycloakspringbootadapter/KeycloakSpringBootAdapterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.keycloakspringbootadapter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeycloakSpringBootAdapterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
